27be9697dcd0a075811145ae063026edd0288840
hokyung <ghrud92@gmail.com>
2018-12-18 23:35:41 +0900

    acmicpc 9012 괄호 성공

---
+++ acmicpc9012.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define OPEN 1
+#define CLOSE 2
+
+int main() {
+    int n;
+    vector<char> stack;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string line;
+        cin >> line;
+        bool success = true;
+        for (int j = 0; j < line.length(); j++) {
+            if (line[j] == '(') {
+                stack.push_back('(');
+            } else if (stack.size() != 0 && stack.back() == '(') {
+                stack.pop_back();
+            } else {
+                success = false;
+                break;
+            }
+        }
+        if (stack.size() > 0) {
+            success = false;
+        }
+        if (success) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+        stack.clear();
+    }
+
+    return 0;
+}
